da836c91e333e8bf7f8f6f92258c214749dfb61c
FelipeFidalgo19 <felipefidalgo100@gmail.com>
2023-06-23 08:00:52 -0300

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,21 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
+  const a = "A";
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
+        {a == "A" ?
+          <p>
+            Aluno&nbsp;
+            <code className={styles.code}>src/app/page.js</code>
+          </p>:
+           <p>
+           Gerente&nbsp;
+           <code className={styles.code}>src/app/page.js</code>
+         </p>
+        }
+
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -22,7 +30,7 @@ export default function Home() {
               className={styles.vercelLogo}
               width={100}
               height={24}
-              priority
+              priority 
             />
           </a>
         </div>
