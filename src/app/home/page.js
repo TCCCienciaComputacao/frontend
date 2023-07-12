06a385aa0f7fe527309b5b22e00f03ee6c4a1cb4
FelipeFidalgo19 <felipefidalgo100@gmail.com>
2023-07-12 07:26:13 -0300

    rotas

---
+++ src/app/home/page.js
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+  <div className='container'>
+    <h1>hello world</h1>
+  </div>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,40 @@
-import styles from './login.css'
+'use client'
 
+import React, { useState } from 'react';
+import logo from './assets/logo.svg';
+import styles from './styles/login.css';
 
-export default function Home() {
-  return (
-  <div className='container'>
-    <h1>hello world</h1>
-  </div>
-  )
+export default function Home(){
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    return(
+        <div className="container">
+            <div className="container-login">
+                <div className="wrap-login">
+                    <form className="login-form" action='http://localhost:3000/home'>
+                        <span className="login-form-title">Seja bem vindo!</span>
+                        <span className="login-form-title-img">
+                            <img src={logo} alt="" />
+                        </span>
+
+                        <div className="wrap-input">
+                           <input className={email !== "" ? "has-val input" : "input"} type="email" value={email} onChange={e => setEmail(e.target.value)}/>
+                           <span className="focus-input" data-placeholder="email"></span>
+                        </div>
+                        <div className="wrap-input">
+                           <input className={password !== "" ? "has-val input" : "input"} type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+                           <span className="focus-input" data-placeholder="senha"></span>
+                        </div>
+
+                        <div className='container-login-form-btn'>
+                            <button className='login-form-btn'>Login</button>
+                        </div>
+                        
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+    
 }
