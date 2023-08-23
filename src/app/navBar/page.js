135e8459f50ea5e29af609770a076c25b18011ca
ViniciusCircuncisao <viniciusfelipe2015.vc@gmail.com>
2023-08-22 22:39:30 -0300

    Create Navbar

---
+++ src/app/navBar/page.js
@@ -0,0 +1,43 @@
+'use client'
+
+import '../styles/navBar.css'
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import React, {useState} from 'react';
+import Link from 'next/link'
+import {SidebarData} from './SidebarData'
+
+export default function navBar(){
+    const [sidebar, setSideBar] = useState(false);
+    const showSidebar = () => setSideBar(!sidebar);
+
+    return(
+        <>
+        <div className='navbar'>
+            <Link href='#' className='menu-bars'>
+                <FaIcons.FaBars onClick={showSidebar} />
+            </Link>
+        </div><nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+                <ul className='nav-menu-items' onClick={showSidebar}>
+                    <li className='navbar-toggle'>
+                        <Link href='#' className='menu-bars'>
+                            <AiIcons.AiOutlineClose/>
+                        </Link>
+                    </li>
+                  {SidebarData.map((item,index) => {
+                    return(
+                     <li key={index} className={item.cName}>
+                        <Link href={item.path}>
+                            {item.icon}
+                            <span>{item.title}</span>
+                        </Link>
+                     </li>
+                    )
+                  })}
+
+                </ul>
+            </nav>
+
+            </>
+    );
+}
