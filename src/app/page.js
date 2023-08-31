5e029f7667f49a10c0ae073920c945bf86cd0f14
Vinicius Circuncisao - Siplan <vinicius.circuncisao@siplan.com.br>
2023-08-31 15:03:46 -0300

    insert validation on login

--- src/app/page.js
+++ src/app/page.js
@@ -5,28 +5,74 @@ import logo from './assets/logo.svg';
 import styles from './styles/login.css';
 
 export default function Home(){
+
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [error, setError] = useState(""); // Estado para a mensagem de erro
+    const [showError, setShowError] = useState(false); // Estado para controlar a exibição da mensagem
+
+    const validateForm = () => {
+        let valid = true;
+    
+        if (email.trim() === "") {
+          setError("* Campo de email é obrigatório");
+          valid = false;
+        } else if (password.trim() === "") {
+          setError("* Campo de senha é obrigatório");
+          valid = false;
+        } else {
+          setError(""); // Limpa a mensagem de erro se não houver erro
+        }
+    
+        return valid;
+      };
+    
+      const handleLogin = (e) => {
+        e.preventDefault();
+    
+        if (validateForm()) {
+          // Lógica de login aqui, redirecione ou faça o que for necessário
+          console.log("Login bem-sucedido!");
+        } else {
+          setShowError(true); // Exibe a mensagem de erro se houver erro
+        }
+      };
+    
+      const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+        setShowError(false); // Oculta a mensagem de erro quando o usuário começa a preencher o campo de email
+      };
+    
+      const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+        setShowError(false); // Oculta a mensagem de erro quando o usuário começa a preencher o campo de senha
+      };
+    
+      
 
     return(
+        
         <div className="container">
             <div className="container-login">
                 <div className="wrap-login">
-                    <form className="login-form" action='http://localhost:3000/home'>
+                    <form className="login-form" action='http://localhost:3000/home' onSubmit={handleLogin}>
                         <span className="login-form-title">Seja bem vindo!</span>
                         <span className="login-form-title-img">
                             <img src={logo} alt="" />
                         </span>
 
                         <div className="wrap-input">
-                           <input className={email !== "" ? "has-val input" : "input"} type="email" value={email} onChange={e => setEmail(e.target.value)}/>
+                           <input className={email !== "" ? "has-val input" : "input"} type="email" value={email} onChange={handleEmailChange}/>
                            <span className="focus-input" data-placeholder="email"></span>
                         </div>
+                        
                         <div className="wrap-input">
-                           <input className={password !== "" ? "has-val input" : "input"} type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+                           <input className={password !== "" ? "has-val input" : "input"} type="password" value={password} onChange={handlePasswordChange}/>
                            <span className="focus-input" data-placeholder="senha"></span>
                         </div>
-
+                            
+                        {showError && <span className="error-message">{error}</span>}
+                        
                         <div className='container-login-form-btn'>
                             <button className='login-form-btn'>Login</button>
                         </div>
@@ -35,6 +81,7 @@ export default function Home(){
                 </div>
             </div>
         </div>
+
     );
     
 }
