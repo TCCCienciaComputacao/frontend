a4c40d03165f971dc09211957c0b3b9777ac1ee5
ViniciusCircuncisao <viniciusfelipe2015.vc@gmail.com>
2023-09-30 02:19:10 -0300

    create page users

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -1,17 +1,38 @@
-import React from "react";
-import axios from "axios";
+'use client'
+
+import react, { useEffect, useState } from "react";
 import {FaTrash , FaEdit} from "react-icons";
-import react from "react";
-import {toast} from "react-toastify";
+import {toast, ToastContainer} from "react-toastify";
+import axios from "axios";
+import '../styles/users.css'
+import  Form from '../users/form';
+import Grid from '../users/grid'
 
 export default function users(){
+    const [users, setUsers] = useState([]);
+    const [onEdit, setOnEdit] = useState(null);
+
+    const getUsers = async () => {
+        try{
+            const res = await axios.get("http://localhost:3000/users");
+            setUsers(res.data.sort((a,b) => (a.nome > b.nome  ? 1 : -1)))
+            console.log(res);
+        }catch(error){
+            toast.error(error);
+        }
+    };
+    useEffect(() => {
+        getUsers();
+
+    }, [setUsers]);
+
+
     return(
-        <table className="tableUsers">
-            <thead>
-                <tr>
-                    <th>Nome</th>
-                </tr>
-            </thead>
-        </table>
+        <div className="container">
+            <h2>USUÁRIOS</h2>
+            <Form onEdit={onEdit} setOnEdit={setOnEdit} getUsers={getUsers} />
+            <Grid setOnEdit={setOnEdit} users={users} setUsers={setUsers} />
+        </div>
+        
     );
 }
